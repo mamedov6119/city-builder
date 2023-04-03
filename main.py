f16fb9fbd9864ef5ac859891a3a42192777496ff
Murad Mammadov <ylin2v@inf.elte.hu>
2023-04-04 01:06:54 +0200

    Class human, check collision and test moving

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 import arcade, arcade.gui, datetime;
-
+import random
+import math
 BLOCK_SIZE = 30
 SCREEN_WIDTH = 780
 SCREEN_HEIGHT = 480
 SCREEN_TITLE = "City-Builder"
+CHRACTER_SCALING = 0.04
+
+def check_boundary_collision(sprite, screen_width, screen_height):
+    if sprite.right > screen_width:
+        sprite.right = screen_width
+    elif sprite.left < 3*BLOCK_SIZE:
+        sprite.left = 3*BLOCK_SIZE
+    if sprite.top > screen_height - BLOCK_SIZE:
+        sprite.top = screen_height - BLOCK_SIZE
+    elif sprite.bottom < 0:
+        sprite.bottom = 0
+
+
+    
+
+class Human(arcade.Sprite):
+    def __init__(self, filename, sprite_scaling):
+        super().__init__(filename, sprite_scaling)
+        self.circle_angle = 0
+        self.circle_radius = 0
+        self.circle_speed = 0.008
+        self.circle_center_x = 0
+        self.circle_center_y = 0
+        
+
+    def update(self):
+        self.center_x = self.circle_radius * math.sin(self.circle_angle) \
+            + self.circle_center_x
+        self.center_y = self.circle_radius * math.cos(self.circle_angle) \
+            + self.circle_center_y
+
+        self.circle_angle += self.circle_speed
+
+        check_boundary_collision(self, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+
+
 
 class Game(arcade.Window):
     """
@@ -14,6 +52,8 @@ class Game(arcade.Window):
         self.boost = 1
         self.items = []
         self.money = 1000
+        self.humans_sprites = None
+        self.humans = []
 
         super().__init__(width, height, title, False)
         arcade.set_background_color(arcade.color.AMAZON)
@@ -53,6 +93,13 @@ class Game(arcade.Window):
         arcade.draw_rectangle_filled((3*BLOCK_SIZE)/2, SCREEN_HEIGHT/2, 3*BLOCK_SIZE, SCREEN_HEIGHT, arcade.color.DARK_GRAY)
         arcade.draw_rectangle_filled(SCREEN_WIDTH/2, SCREEN_HEIGHT - (BLOCK_SIZE/2), SCREEN_WIDTH, BLOCK_SIZE, arcade.color.GRAY)
 
+        # test boundaries rectangle
+        # arcade.draw_rectangle_filled(SCREEN_WIDTH/2+1.5*BLOCK_SIZE, SCREEN_HEIGHT/2-BLOCK_SIZE, SCREEN_WIDTH-3*BLOCK_SIZE, SCREEN_HEIGHT-BLOCK_SIZE, arcade.color.BLACK)
+
+
+
+
+
         # Money 
         arcade.draw_text(f"Funds: ${self.money:,}", 10, SCREEN_HEIGHT - 20, arcade.color.WHITE, 14, font_name="Courier")
 
@@ -70,8 +117,18 @@ class Game(arcade.Window):
             arcade.draw_line(BLOCK_SIZE*3, y, SCREEN_WIDTH, y, arcade.color.GRAY, 1)
 
     def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
-        pass
+        
+        self.humans_sprites = arcade.SpriteList()
+        
+        for i in range(10):
+            human = Human("spaceman.png", CHRACTER_SCALING)
+            human.circle_center_y = random.randrange(150, 700)
+            human.circle_center_x = random.randrange(150, 700)
+            human.circle_radius = random.randrange(10, 200)
+            human.circle_angle = random.random() * 2 * math.pi
+            self.humans.append(human)
+            self.humans_sprites.append(human)
+
 
     def on_draw(self):
         """ Render the screen. """
@@ -81,6 +138,11 @@ class Game(arcade.Window):
         self.menu_manager.draw()
         self.sidetop_manager.draw()
         self.sidebtm_manager.draw()
+        self.humans_sprites.draw()
+        
+
+        
+   
 
     def on_update(self, delta_time):
         """
@@ -89,6 +151,19 @@ class Game(arcade.Window):
         need it.
         """
 
+        self.humans_sprites.update()
+        
+       
+        
+        
+        
+    def on_key_press(self, key, modifiers):
+        """
+        Called whenever a key is pressed.
+        """
+    
+        
+
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """ Called whenever the mouse moves. """
         pass # hover, if building is selected
@@ -96,7 +171,15 @@ class Game(arcade.Window):
     def on_mouse_press(self, x, y, button, key_modifiers):
         """ Called when the user presses a mouse button. """
         print(x//BLOCK_SIZE,y//BLOCK_SIZE)
+        # make the humans move
+       
 
+
+
+
+    
+    
+    
 def main():
     """ Main function """
     game = Game()
@@ -106,3 +189,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
